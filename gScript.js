9fd51c128588cd337a588ba7dc80f04b8e2e7154
Sk Juned <148184794+Skjuned-89@users.noreply.github.com>
2025-07-19 01:27:53 +0530

    Update gScript.js

--- gScript.js
+++ gScript.js
@@ -1,5 +1,46 @@
-gsap.from(".bannerSection .bannerContent", {
+gsap.from(".bannerContent", {
     duration: 1,
     opacity: 0,
     x: -150
+});
+gsap.from(".broadBandCard1 ,.broadBandCard2 ,.broadBandCard3", {
+    scale: 0,
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".broadBandCard1 ,.broadBandCard2 ,.broadBandCard3",
+        scroller: "body",
+        start: "top 70%"
+    }
+});
+gsap.from(".aboutUsContent", {
+    x: -150,
+    opacity: 0,
+    duration: 1,
+    markers: true,
+    scrollTrigger: {
+        trigger: ".aboutUsContent",
+        scroller: "body",
+        start: "top 30%"
+    }
+});
+gsap.from(".aboutUsSection .container .aboutImage", {
+    y: -100,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".aboutUsSection .container .aboutImage",
+        scroller: "body",
+        start: "bottom 92%"
+    }
+});
+gsap.from(".serviceSection .container ul", {
+    scale: 0,
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".serviceSection .container ul",
+        scroller: "body",
+        start: "top 90%"
+    }
 });
