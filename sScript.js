f5b7eabfb95bcf0eac19d6a03f868ee90bcfd27c
Sk Juned <148184794+Skjuned-89@users.noreply.github.com>
2025-07-21 12:44:00 +0530

    Update sScript.js

--- sScript.js
+++ sScript.js
@@ -1,1 +1,44 @@
+let swiper = new Swiper(".container-wrapper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});let headerCont = document.querySelector("Header .container")
+document.addEventListener('scroll', () => {
+    console.log("scrolling")
+    if (window.scrollY > 0) {
+        headerCont.style.paddingTop = "10px"
+        headerCont.parentElement.style.backgroundColor = "#1c262f"
+    } else {
+        headerCont.style.paddingTop = "30px"
+        headerCont.parentElement.style.backgroundColor = "transparent"
+    }
+
+})
 
