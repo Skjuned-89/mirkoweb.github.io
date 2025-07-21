46e7e94eba0cac8b5e8420594f8910ac51f922ce
Sk Juned <148184794+Skjuned-89@users.noreply.github.com>
2025-07-22 02:41:43 +0530

    Update sScript.js

--- sScript.js
+++ sScript.js
@@ -29,16 +29,4 @@ let swiper = new Swiper(".container-wrapper", {
             spaceBetween: 50,
         },
     },
-});let headerCont = document.querySelector("Header .container")
-document.addEventListener('scroll', () => {
-    console.log("scrolling")
-    if (window.scrollY > 0) {
-        headerCont.style.paddingTop = "10px"
-        headerCont.parentElement.style.backgroundColor = "#1c262f"
-    } else {
-        headerCont.style.paddingTop = "30px"
-        headerCont.parentElement.style.backgroundColor = "transparent"
-    }
-
-})
-
+});
